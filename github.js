125bea1f1b64524a7d540a6b07e26a97495829b8
Alejandro Leiva <aleiva@emergya.com>
2012-03-28 00:11:53 +0200

    Creating initial infrastructure.

---
+++ github.js
@@ -0,0 +1,32 @@
+/*
+ * Connector to Redmine from Google Apps Scripts platform.
+ *
+ * Copyright (c) 2011,2012 Emergya
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Author: Alejandro Leiva <aleiva@emergya.com>
+ *
+ */
+
+var GITHUB_URL = 'http://github.com';
+
+// TODO: this should be obtained from a configuration dialog.
+var API_ACCESS_KEY = 'YOUR_API_ACCESS_KEY_HERE!';
+
+var Github = (function() {
+
+  function Github() {
+  }
+
+  return Github;
+
+})();
